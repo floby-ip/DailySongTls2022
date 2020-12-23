a04282566e93a3b0ca72dad6fe9dcaa01dbac783
simong <simonguillochon@outlook.fr>
2020-12-23 12:28:35 +0100

    refactor: remove ids for day content and unused vue router

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="grid">
     <v-row>
-      <v-col cols="12" sm="4" v-for="col in dayContent" :key="col.id">
+      <v-col cols="12" sm="4" v-for="(link, index) in dayContent" :key="index">
         <v-card class="mx-4 my-6 pa-1" height="30vh" color="darker" flat>
           <iframe
             class="embed"
             width="100%"
             height="100%"
-            :src="col.link"
+            :src="link"
             frameborder="0"
           ></iframe>
         </v-card>
